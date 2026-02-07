e5ddf0eeac7104a37ec67543db3b619d381d20d5
CurroAyala <curroayala18@gmail.com>
2026-02-07 21:45:41 +0100

    fragment de ajustes añadido

---
+++ app/src/main/java/com/curro/movietracker/ui/settings/SettingsFragment.java
@@ -0,0 +1,41 @@
+package com.curro.movietracker.ui.settings;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.core.os.LocaleListCompat;
+import androidx.fragment.app.Fragment;
+
+import com.curro.movietracker.databinding.FragmentSettingsBinding;
+
+public class SettingsFragment extends Fragment {
+
+    private FragmentSettingsBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = FragmentSettingsBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.btnSpanish.setOnClickListener(v -> changeLanguage("es"));
+
+        binding.btnEnglish.setOnClickListener(v -> changeLanguage("en"));
+    }
+
+    private void changeLanguage(String languageCode) {
+        LocaleListCompat appLocale = LocaleListCompat.forLanguageTags(languageCode);
+        AppCompatDelegate.setApplicationLocales(appLocale);
+    }
+
+}
